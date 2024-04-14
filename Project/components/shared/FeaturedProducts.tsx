0eaa83664346a676133baf5cc287efac219b3951
saadrehman171000 <saadrehman1710000@gmail.com>
2024-04-14 12:26:02 +0500

    blogs card

--- Project/components/shared/FeaturedProducts.tsx
+++ Project/components/shared/FeaturedProducts.tsx
@@ -14,11 +14,9 @@ import { Button } from "../ui/button";
 const FeaturedProducts = async () => {
   const featuredProducts = await getFeaturedProducts();
 
-  console.log(featuredProducts.data);
-
   return (
     <div className="my-52">
-      <h1 className="text-center font-extrabold tracking-tighter text-[2rem]">
+      <h1 className="text-center font-extrabold tracking-tighter text-2xl md:text-[2rem]">
         Check out our{" "}
         <span className="bg-gradient-to-r from-red-700 via-red-500 to-red-400 inline-block text-transparent bg-clip-text">
           Featured Products
@@ -26,11 +24,11 @@ const FeaturedProducts = async () => {
       </h1>
       <div className="max-w-7xl mx-auto p-5 px-10 w-full my-2">
         <div className="flex flex-col items-center justify-center gap-10">
-          <div className="flex gap-5 items-center justify-center mt-5">
+          <div className="flex flex-col md:flex-row gap-5 items-center justify-center mt-5">
             {featuredProducts.data?.map((data) => (
               <Card
                 key={data.id}
-                className="bg-white rounded-2xl shadow-2xl px-10 py-10 hover:scale-105 transition-all duration-300 w-[300px]"
+                className="bg-white rounded-2xl shadow-2xl px-10 py-10 w-[300px]"
               >
                 <CardContent className="flex flex-col gap-7">
                   <Image
--- Project/components/shared/Navbar.tsx
+++ Project/components/shared/Navbar.tsx
@@ -10,6 +10,19 @@ import {
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
 import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+
+import menu from "@/public/menu.svg";
+
 const Navbar = ({ dashboard }: { dashboard: boolean }) => {
   const TOP_OFFSET = 50;
   const { isAuthenticated } = useKindeBrowserClient();
@@ -44,79 +57,149 @@ const Navbar = ({ dashboard }: { dashboard: boolean }) => {
             Handphone
           </Link>
         </div>
-        <div className="flex gap-10">
-          <Link
-            className={`${pathname === "/" ? "font-bold" : "text-black"}`}
-            href={"/"}
-          >
-            HOME
-          </Link>
-          <Link
-            className={`${pathname === "/page" ? "font-bold" : "text-black"}`}
-            href={"/page"}
-          >
-            PAGE
-          </Link>
-          <Link
-            className={`${pathname === "/brand" ? "font-bold" : "text-black"}`}
-            href={"/brand"}
-          >
-            BRAND
-          </Link>
-          <Link
-            className={`${pathname === "/blog" ? "font-bold" : "text-black"}`}
-            href={"/blog"}
-          >
-            BLOG
-          </Link>
-          <Link
-            className={`${pathname === "/about" ? "font-bold" : "text-black"}`}
-            href={"/about"}
-          >
-            ABOUT
-          </Link>
-          <Link
-            className={`${
-              pathname === "/contact" ? "font-bold" : "text-black"
-            }`}
-            href={"/contact"}
-          >
-            CONTACT
-          </Link>
-        </div>
-        <div className="flex gap-4">
-          <Search />
-          <ShoppingCart />
-          <div>
-            {!isAuthenticated ? (
-              <div className="flex gap-2">
-                <LoginLink className="rounded-2xl border px-3 py-1">
-                  Sign In
-                </LoginLink>
-                <RegisterLink className="rounded-2xl border bg-black px-3 py-1 text-white">
-                  {" "}
-                  Sign up
-                </RegisterLink>
-              </div>
-            ) : (
-              <div className="flex gap-2 items-center justify-center">
-                {!dashboard ? (
-                  <h1 className="font-bold tracking-tight text-lg">Welcome!</h1>
-                ) : (
-                  <Link
-                    href={"/dashboard"}
-                    className="bg-black rounded-2xl text-white px-3 py-1"
-                  >
-                    Dashboard
-                  </Link>
-                )}
-
-                <LogoutLink className="rounded-2xl border px-3 py-1 bg-black text-white">
-                  Logout
-                </LogoutLink>
-              </div>
-            )}
+        <div className="hidden md:block">
+          <div className="flex gap-10">
+            <Link
+              className={`${pathname === "/" ? "font-bold" : "text-black"}`}
+              href={"/"}
+            >
+              HOME
+            </Link>
+            <Link
+              className={`${pathname === "/page" ? "font-bold" : "text-black"}`}
+              href={"/page"}
+            >
+              PAGE
+            </Link>
+            <Link
+              className={`${
+                pathname === "/brand" ? "font-bold" : "text-black"
+              }`}
+              href={"/brand"}
+            >
+              BRAND
+            </Link>
+            <Link
+              className={`${pathname === "/blog" ? "font-bold" : "text-black"}`}
+              href={"/blog"}
+            >
+              BLOG
+            </Link>
+            <Link
+              className={`${
+                pathname === "/about" ? "font-bold" : "text-black"
+              }`}
+              href={"/about"}
+            >
+              ABOUT
+            </Link>
+            <Link
+              className={`${
+                pathname === "/contact" ? "font-bold" : "text-black"
+              }`}
+              href={"/contact"}
+            >
+              CONTACT
+            </Link>
           </div>
+        </div>
+        <div className="block md:hidden">
+          <Sheet>
+            <SheetTrigger className="align-middle">
+              <Image
+                src={menu}
+                alt="menu"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+              <Link href={"/"} className="tracking-tight font-bold text-lg">
+                Handphone
+              </Link>
+              <Separator className="border border-gray-50" />
+              <div className="flex flex-col gap-5">
+                <Link
+                  className={`${pathname === "/" ? "font-bold" : "text-black"}`}
+                  href={"/"}
+                >
+                  HOME
+                </Link>
+                <Link
+                  className={`${
+                    pathname === "/page" ? "font-bold" : "text-black"
+                  }`}
+                  href={"/page"}
+                >
+                  PAGE
+                </Link>
+                <Link
+                  className={`${
+                    pathname === "/brand" ? "font-bold" : "text-black"
+                  }`}
+                  href={"/brand"}
+                >
+                  BRAND
+                </Link>
+                <Link
+                  className={`${
+                    pathname === "/blog" ? "font-bold" : "text-black"
+                  }`}
+                  href={"/blog"}
+                >
+                  BLOG
+                </Link>
+                <Link
+                  className={`${
+                    pathname === "/about" ? "font-bold" : "text-black"
+                  }`}
+                  href={"/about"}
+                >
+                  ABOUT
+                </Link>
+                <Link
+                  className={`${
+                    pathname === "/contact" ? "font-bold" : "text-black"
+                  }`}
+                  href={"/contact"}
+                >
+                  CONTACT
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        <div>
+          {!isAuthenticated ? (
+            <div className="flex gap-2">
+              <LoginLink className="rounded-2xl border px-3 py-1">
+                Sign In
+              </LoginLink>
+              <RegisterLink className="rounded-2xl border bg-black px-3 py-1 text-white">
+                {" "}
+                Sign up
+              </RegisterLink>
+            </div>
+          ) : (
+            <div className="flex gap-2 items-center justify-center">
+              {!dashboard ? (
+                <h1 className="font-bold tracking-tight text-lg">Welcome!</h1>
+              ) : (
+                <Link
+                  href={"/dashboard"}
+                  className="bg-black rounded-2xl text-white px-3 py-1"
+                >
+                  Dashboard
+                </Link>
+              )}
+
+              <LogoutLink className="rounded-2xl border px-3 py-1 bg-black text-white">
+                Logout
+              </LogoutLink>
+            </div>
+          )}
         </div>
       </div>
     </div>
