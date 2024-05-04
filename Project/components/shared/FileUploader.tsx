9274f80450feafb5fa16be9547503bb7d5ef84bb
Ali Shaikh <101398958+shaiiikh@users.noreply.github.com>
2024-05-04 17:24:48 +0500

    Update FileUploader.tsx

--- Project/components/shared/FileUploader.tsx
+++ Project/components/shared/FileUploader.tsx
@@ -1,5 +1,5 @@
-"use client";
 
+"use client";
 import React, { Dispatch, SetStateAction, useCallback } from "react";
 import { useDropzone } from "@uploadthing/react/hooks";
 import { Button } from "../ui/button";
