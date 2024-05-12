acd069862f1244cfcdb77f2783a2add4198e2dc0
saadrehman171000 <saadrehman1710000@gmail.com>
2024-05-12 23:18:57 +0500

    routing in page.tsx

--- Project/app/(dashboard)/edit-contact/page.tsx
+++ Project/app/(dashboard)/edit-contact/page.tsx
@@ -1,16 +1,15 @@
-import ContactUs from "@/components/shared/ContactUs";
+import GetContact from "@/components/shared/GetContact";
 import React from "react";
 
 const EditContactPage = () => {
   return (
     <div className="flex flex-col gap-10 items-start ">
       <h1 className="font-bold text-center tracking-tighter text-3xl ">
-        Edit Contact Page
+        View Contact Request
       </h1>
-      <ContactUs />
+      <GetContact />
     </div>
   );
 };
-
 
 export default EditContactPage;
